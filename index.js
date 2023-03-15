b3489ad4ef03361b898fb2fb0492c2eb6c3821f9
Joy-GitHuub <github.joy@gmail.com>
2023-03-15 08:22:30 +0600

    grocery-market-place

--- index.js
+++ index.js
@@ -8,22 +8,6 @@ const colors = require('colors');
 
 mongoose.set('strictQuery', false)
 
-// const db = `mongodb+srv://watch-me-project:watch-me-project@cluster0.garvawt.mongodb.net/?retryWrites=true&w=majority`;
-// const db = `mongodb+srv://Market_Place:Market_Place@cluster0.s4ariea.mongodb.net/?retryWrites=true&w=majority`;
-/* 
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000,
-    autoIndex: false, // Don't build indexes
-    maxPoolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
-} */
-// mongoose.connect(db).then(() => {
-//     console.log(`Watch-ME Database Connection is Successful`.green.bold);
-// });
 mongoose.connect(process.env.DB).then(() => {
     console.log(`MARKET-PLACE Database Connection is Successful`.green.bold);
 });
